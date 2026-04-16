e7ae47cb32742b45508198ab2ec0def1dae9e6f4
whatsafterguidee <asamapohn.val@dome.tu.ac.th>
2026-04-16 17:11:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ Your task is to review the provided `Source Text` and its initial `Machine Trans
 
 # ตั้งค่าโมเดล
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
+    model_name="gemini-1.5-pro-latest",
     system_instruction=system_instruction
 )
 
